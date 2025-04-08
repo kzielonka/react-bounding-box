7f598338865a6be63d8d4d3cf073b9c321d49039
Krzysztof Zielonka <aknolezik@gmail.com>
2025-04-08 20:23:03 +0200

    Added simple rectangle on the canvas

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 export interface Props {
   src: string;
 }
 
 const BoundingBoxImage: React.FC<Props> = ({ src }: Props) => {
-  return <div><div>Hello World!!!</div><img src={src} /></div>;
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    const ctx = canvasRef.current?.getContext("2d");
+    if (!ctx) {
+      return;
+    }
+    ctx.strokeStyle = "blue"; 
+    ctx.lineWidth = 3;
+    ctx.strokeRect(50, 50, 150, 100); 
+  }, [canvasRef]);
+
+  return (
+    <div>
+        <div>Hello World!!!</div>
+        <img src={src} />
+        <canvas ref={canvasRef} width="1000" height="1000" />
+    </div>
+  );
 };
 
 export default BoundingBoxImage;
